33217cdcb51cbe739ec893cf93864cc08cb0741c
sohm-mandal <soham.mandal24@gmail.com>
2024-07-06 02:37:28 +0530

    first

---
+++ main1.py
@@ -0,0 +1,171 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import img_to_array
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer, VideoProcessorBase, RTCConfiguration, WebRtcMode
+import av
+
+st.title("LIVE SENTIMENT ANALYSIS 😄 😡 😞 😲 🤢 😨 😐")
+load_model=tf.keras.models.load_model(r'final_model.h5')
+
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+emotion_dict = {0: 'Angry', 1: 'Disgust', 2: 'Happy', 3: 'Sad', 4: 'Neutral', 5: 'Surprised'}
+
+RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
+
+
+font_scale=1.5
+font=cv2.FONT_HERSHEY_PLAIN
+
+class VideoProcessor:
+    def recv(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(image=img_gray, scaleFactor=1.1, minNeighbors=3)
+
+        face_roi=img
+
+        for (x, y, w, h) in faces:
+                cv2.rectangle(img=img, pt1=(x, y), pt2=(x + w, y + h), color=(0, 255, 0), thickness=3)
+                roi_gray=img_gray[y:y+h, x:x+w]
+                roi_color=img[y:y+h, x:x+w]
+                
+                facess=faceCascade.detectMultiScale(roi_gray)
+                if(len(facess)==0):
+                        print("Face not detected")
+                else:
+                        for(ex,ey,ew,eh) in facess:
+                                face_roi=roi_color[ey:ey+eh, ex:ex + ew]
+
+        final_image=cv2.resize(face_roi, (224,224)) # resizing the image to fit it in the model
+        final_image=np.expand_dims(final_image,axis=0) # need the fourth dimension
+        final_image=final_image/255.0 #normalizing the data
+
+
+        predictions=load_model.predict(final_image)
+
+
+        if(np.argmax(predictions)==0):
+                status="Angry"
+
+                x1,y1,w1,h1=0,0,175,75
+
+                cv2.rectangle(img, (x1,x1), (x1 + w1, y1 + h1), (0,0,0), -1)
+
+                cv2.putText(img,status,(x1 + int(w1/10), y1 + int(h1/2)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+
+                cv2.putText(img,status,(100,150), font, 3, (0,0,255),2,cv2.LINE_4)
+                
+                cv2.rectangle(img,(x1,y1), (x1+w1, y1+h1), (0,0,255))
+
+
+        elif(np.argmax(predictions)==1):
+                status="Disgust"
+
+                x1,y1,w1,h1=0,0,175,75
+
+                cv2.rectangle(img, (x1,x1), (x1 + w1, y1 + h1), (0,0,0), -1)
+
+                cv2.putText(img,status,(x1 + int(w1/10), y1 + int(h1/2)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+
+                cv2.putText(img,status,(100,150), font, 3, (0,0,255),2,cv2.LINE_4)
+
+                cv2.rectangle(img,(x1,y1), (x1+w1, y1+h1), (0,0,255))    
+
+       
+
+        elif(np.argmax(predictions)==2):
+                status="Fear"
+
+                x1,y1,w1,h1=0,0,175,75
+
+                cv2.rectangle(img, (x1,x1), (x1 + w1, y1 + h1), (0,0,0), -1)
+
+                cv2.putText(img,status,(x1 + int(w1/10), y1 + int(h1/2)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+
+                cv2.putText(img,status,(100,150), font, 3, (0,0,255),2,cv2.LINE_4)
+
+                cv2.rectangle(img,(x1,y1), (x1+w1, y1+h1), (0,0,255))    
+
+       
+
+        elif(np.argmax(predictions)==3):
+                status="Happy"
+
+                x1,y1,w1,h1=0,0,175,75
+
+                cv2.rectangle(img, (x1,x1), (x1 + w1, y1 + h1), (0,0,0), -1)
+
+                cv2.putText(img,status,(x1 + int(w1/10), y1 + int(h1/2)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+
+                cv2.putText(img,status,(100,150), font, 3, (0,0,255),2,cv2.LINE_4)
+
+                cv2.rectangle(img,(x1,y1), (x1+w1, y1+h1), (0,0,255))    
+
+       
+
+        elif(np.argmax(predictions)==4):
+                status="Sad" 
+
+                x1,y1,w1,h1=0,0,175,75
+
+                cv2.rectangle(img, (x1,x1), (x1 + w1, y1 + h1), (0,0,0), -1)
+
+                cv2.putText(img,status,(x1 + int(w1/10), y1 + int(h1/2)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+
+                cv2.putText(img,status,(100,150), font, 3, (0,0,255),2,cv2.LINE_4)
+
+                cv2.rectangle(img,(x1,y1), (x1+w1, y1+h1), (0,0,255))    
+
+       
+
+        elif(np.argmax(predictions)==5):
+                status="Neutral"
+
+                x1,y1,w1,h1=0,0,175,75
+
+                cv2.rectangle(img, (x1,x1), (x1 + w1, y1 + h1), (0,0,0), -1)
+
+                cv2.putText(img,status,(x1 + int(w1/10), y1 + int(h1/2)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+
+                cv2.putText(img,status,(100,150), font, 3, (0,0,255),2,cv2.LINE_4)
+
+                cv2.rectangle(img,(x1,y1), (x1+w1, y1+h1), (0,0,255))    
+
+       
+
+        elif(np.argmax(predictions)==6):
+                status="Surprised"
+
+                x1,y1,w1,h1=0,0,175,75
+
+                cv2.rectangle(img, (x1,x1), (x1 + w1, y1 + h1), (0,0,0), -1)
+
+                cv2.putText(img,status,(x1 + int(w1/10), y1 + int(h1/2)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+
+                cv2.putText(img,status,(100,150), font, 3, (0,0,255),2,cv2.LINE_4)
+
+                cv2.rectangle(img,(x1,y1), (x1+w1, y1+h1), (0,0,255))
+        
+        return av.VideoFrame.from_ndarray(img,format="bgr24")
+
+def main():
+    st.write("Select preferred input device and click Start.")
+    webrtc_streamer(key="key", rtc_configuration=RTC_CONFIGURATION, video_processor_factory=VideoProcessor)
+
+    html_temp4 = """
+                        <div style="padding:10px">
+                        <h4 style="color:white;text-align:center;">Developed by Rishab Mahato using OpenCV,Streamlit,Tensorflow and Keras.</h4>
+                        <h4 style="color:white;text-align:center;">Thanks for Visiting</h4>
+                        </div>
+                        <a href="https://github.com/Rishab7988">Github</a>
+                        <a href="https://www.linkedin.com/in/rishab-mahato/">LinkedIn</a>
+                        <br></br>
+                        <br></br>"""
+
+    st.markdown(html_temp4, unsafe_allow_html=True)
+
+if __name__ == "__main__":
+    main()
---
+++ video_test.py
@@ -0,0 +1,140 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+
+
+# In order to only extract the face from the image, I will be using haarcascade frontal face detection algorithm
+
+# loading my model
+load_model=tf.keras.models.load_model(r'C:\Users\LENOVO\Documents\VIT\CODES\AI ML\NEURAL NETWORK\CNN\project\Emotion detection\final_model.h5')
+
+
+path=r'C:\Users\LENOVO\Documents\VIT\CODES\AI ML\NEURAL NETWORK\CNN\project\Emotion detection\haarcascade_frontalface_default.xml'
+font_scale=1.5
+font=cv2.FONT_HERSHEY_PLAIN
+
+
+#set rectangular background to white
+rectangle_bgr=(225,225,225)
+#make a black image
+img=np.zeros((500,500))
+
+text="Some text in a box!"
+
+(text_width,text_height)=cv2.getTextSize(text,font,fontScale=font_scale,thickness=1)[0]
+
+text_offset_x=10
+text_offset_y=img.shape[0]-25
+
+#make the coords of the box with a small padding of two pixels
+box_coords=((text_offset_x,text_offset_y),(text_offset_x + text_width + 2, text_offset_y - text_height -2))
+cv2.rectangle(img,box_coords[0],box_coords[1],rectangle_bgr,cv2.FILLED)
+cv2.putText(img,text,(text_offset_x,text_offset_y),font,fontScale=font_scale,color=(0,0,0),thickness=1)
+
+
+cap=cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    cap=cv2.VideoCapture(0)
+if not cap.isOpened():
+    raise IOError("Cannot open webcam")
+
+while True:
+    ret,frame=cap.read()
+
+    faceCascade=cv2.CascadeClassifier(r'C:\Users\LENOVO\Documents\VIT\CODES\AI ML\NEURAL NETWORK\CNN\project\Emotion detection\haarcascade_frontalface_default.xml')
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    
+
+    face_roi=frame
+
+    faces=faceCascade.detectMultiScale(gray,1.1,4)
+    
+    for x,y,w,h in faces:
+      roi_gray=gray[y:y+h, x:x+w]
+      roi_color=frame[y:y+h, x:x+w]
+      cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
+
+      
+      facess=faceCascade.detectMultiScale(roi_gray)
+      if(len(facess)==0):
+         print("Face not detected")
+      else:
+         for(ex,ey,ew,eh) in facess:
+            face_roi=roi_color[ey:ey+eh, ex:ex + ew]
+
+    final_image=cv2.resize(face_roi, (224,224)) # resizing the image to fit it in the model
+    final_image=np.expand_dims(final_image,axis=0) # need the fourth dimension
+    final_image=final_image/255.0 #normalizing the data
+    
+
+
+
+    font=cv2.FONT_HERSHEY_SIMPLEX
+
+    predictions=load_model.predict(final_image)
+
+    font_scale=1.5
+    font=cv2.FONT_HERSHEY_PLAIN
+
+
+    if(np.argmax(predictions)==0):
+       status="Angry"
+
+       x1,y1,w1,h1=0,0,175,75
+
+       cv2.rectangle(frame, (x1,x1), (x1 + w1, y1 + h1), (0,0,0), -1)
+
+       cv2.putText(frame,status,(x1 + int(w1/10), y1 + int(h1/2)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+
+       cv2.putText(frame,status,(100,150), font, 3, (0,0,255),2,cv2.LINE_4)
+
+       cv2.rectangle(frame,(x1,y1), (x1+w1, y1+h1), (0,0,255))
+
+    elif(np.argmax(predictions)==1):
+       status="Happy"
+
+       x1,y1,w1,h1=0,0,175,75
+
+       cv2.rectangle(frame, (x1,x1), (x1 + w1, y1 + h1), (0,0,0), -1)
+
+       cv2.putText(frame,status,(x1 + int(w1/10), y1 + int(h1/2)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+
+       cv2.putText(frame,status,(100,150), font, 3, (0,0,255),2,cv2.LINE_4)
+
+       cv2.rectangle(frame,(x1,y1), (x1+w1, y1+h1), (0,0,255))    
+
+    elif(np.argmax(predictions)==2):
+       status="Neutral"
+
+       x1,y1,w1,h1=0,0,175,75
+
+       cv2.rectangle(frame, (x1,x1), (x1 + w1, y1 + h1), (0,0,0), -1)
+
+       cv2.putText(frame,status,(x1 + int(w1/10), y1 + int(h1/2)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+
+       cv2.putText(frame,status,(100,150), font, 3, (0,0,255),2,cv2.LINE_4)
+
+       cv2.rectangle(frame,(x1,y1), (x1+w1, y1+h1), (0,0,255))    
+
+    elif(np.argmax(predictions)==3):
+       status="Surprised"
+
+       x1,y1,w1,h1=0,0,175,75
+
+       cv2.rectangle(frame, (x1,x1), (x1 + w1, y1 + h1), (0,0,0), -1)
+
+       cv2.putText(frame,status,(x1 + int(w1/10), y1 + int(h1/2)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+
+       cv2.putText(frame,status,(100,150), font, 3, (0,0,255),2,cv2.LINE_4)
+
+       cv2.rectangle(frame,(x1,y1), (x1+w1, y1+h1), (0,0,255))    
+
+    cv2.imshow('Face Emotion Recognition',frame)
+
+    if cv2.waitKey(2) & 0xFF == ord('q'):
+       break
+
+cap.release()
+cv2.destroyAllWindows()
+    
